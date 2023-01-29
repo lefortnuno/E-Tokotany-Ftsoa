e2c931f62a57bb0938279781efef7337ae956f40
unknown <lefortnuno@gmail.com>
2023-01-29 21:00:58 +0300

    commit

--- src/contexts/sidebar/navbar.context.js
+++ src/contexts/sidebar/navbar.context.js
@@ -27,56 +27,52 @@ export default function NavbarContext() {
 						<span className="badge badge-count">3</span>
 					</Link>
 				</li>
-				{u_info.u_attribut !== "Usager" ? (
-					<>
-						<li className="nav-item ">
-							<Link to="/dossier/">
-								<i className="">
-									<BsFolder2Open />
-								</i>
-								<p> Dossiers </p>
-								<span className="badge badge-count">1</span>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/individu/">
-								<i>
-									<BsPeople />
-								</i>
-								<p> Personnes </p>
-								<span className="badge badge-count">3</span>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/C_ND/">
-								<i>
-									<BsStickies />
-								</i>
-								<p> Cahiers </p>
-								<span className="badge badge-count">5</span>
-							</Link>
-						</li>
-						<br />
-						<li className="nav-item ">
-							<Link to="/terrain/">
-								<i>
-									<BsGlobe2 />
-								</i>
-								<p>Terrain</p>
-								<span className="badge badge-success">3</span>
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/stats/">
-								<i>
-									<BsReception4 />
-								</i>
-								<p>Statisique</p>
-								<span className="badge badge-info">3</span>
-							</Link>
-						</li>
-					</>
-				) : null}
+				<li className="nav-item ">
+					<Link to="/dossier/">
+						<i className="">
+							<BsFolder2Open />
+						</i>
+						<p> Dossiers </p>
+						<span className="badge badge-count">1</span>
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/individu/">
+						<i>
+							<BsPeople />
+						</i>
+						<p> Personnes </p>
+						<span className="badge badge-count">3</span>
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/C_ND/">
+						<i>
+							<BsStickies />
+						</i>
+						<p> Cahiers </p>
+						<span className="badge badge-count">5</span>
+					</Link>
+				</li>
+				<br />
+				<li className="nav-item ">
+					<Link to="/terrain/">
+						<i>
+							<BsGlobe2 />
+						</i>
+						<p>Terrain</p>
+						<span className="badge badge-success">3</span>
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/stats/">
+						<i>
+							<BsReception4 />
+						</i>
+						<p>Statisique</p>
+						<span className="badge badge-info">3</span>
+					</Link>
+				</li>
 			</ul>
 		</>
 	);
